325ec1f01f5636caba23927ded22465a4830ad32
Ben <rbenrodford@gmail.com>
2015-03-31 19:17:05 -0400

    Parser does more stuff

--- Parser.java
+++ Parser.java
@@ -9,18 +9,22 @@ public class Parser {
 	
 	public static void parse () {
 		String name, description;
-		String[] ingredients;
+		String[] ingredients, directions;
 		try {
-			Document doc = Jsoup.connect("http://www.food.com/recipeprint.do?rid=485824").get();
+			Document doc = Jsoup.connect("http://www.food.com/recipeprint.do?rid=485182").get();
+			
+			name = doc.title().split(" Recipe")[0];
+			
 			Elements meta = doc.select("meta");
 			ingredients = meta.get(2).attr("content").split(" recipe")[0].split("\\,");
 			description = meta.get(3).attr("content");
-			System.out.println(description);
-			for (String s : ingredients) System.out.println(s);
+			Elements dirs = doc.getElementsByTag("ol").get(0).getElementsByTag("span");
+			directions = new String[dirs.size()];
+			for (int i = 0; i < dirs.size(); i++) directions[i] = dirs.get(i).text();
 			
 			
 		} catch (IOException e) {
-			//	break;				for exiting from the loop later
+			//	continue;				for the loop later
 		}
 	}
 	

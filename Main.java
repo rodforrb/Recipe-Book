edb04671bf270b90aeee2f9b38d50573f6107ac7
Ben <rbenrodford@gmail.com>
2015-04-06 09:34:59 -0400

    more changes, demo version

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 package recipebook;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -17,7 +18,7 @@ public class Main {
 	public static void save() throws IOException {
 		// write to file
 		File file = new File("data/recipes");
-		BufferedWriter out = new BufferedWriter(new FileWriter(file));
+		BufferedWriter out = new BufferedWriter(new FileWriter(file, true));
 		ArrayList<RecipeADT> list = recipes.list();
 		for (RecipeADT r : list) {
 			out.write(r.data());
@@ -27,32 +28,51 @@ public class Main {
 	
 	public static void load() throws IOException, FileNotFoundException {
 		// read from file
-		BufferedReader reader = new BufferedReader(new FileReader("data/recipes"));
+		BufferedReader reader = new BufferedReader(new FileReader("data/recipes-short"));
 
 		// read all lines
-		String readLine;
-		String[] data = new String[5];
+		String readLine, name;
+//		String[] data = new String[5];
 		String[] list;
 		ArrayList<String> ingredients, directions;
 		RecipeADT r;
 		while ( (readLine = reader.readLine()) != null) {
+			r = new RecipeADT();
 			// read name
-			data[0] = readLine;
-			// read next four elements
-			for (int i = 1; i < 5; i++) data[i] = reader.readLine();
+			name = readLine;
+			r.setRecipeName(name);
+			r.setPrepTime(reader.readLine());
+			r.setTotalTime(reader.readLine());
+			r.setServingSize(reader.readLine());
+			r.setAboutThisRecipe(reader.readLine());
+//			data[0] = readLine;
+//			// read next four elements
+//			for (int i = 1; i < 5; i++) data[i] = reader.readLine();
 			// read ingredients list
 			ingredients = new ArrayList<String>();
+			r.setIngredients(ingredients);
 			list = reader.readLine().split("\\,\\,");
-			for (String s : list) ingredients.add(s);
+			for (String s : list) {
+				ingredients.add(s);
+				recipesByIngredient.insert(s, r);
+			}
 			// read directions list
 			directions = new ArrayList<String>();
+			r.setDirections(directions);
 			list = reader.readLine().split("\\,\\,");
 			for (String s : list) directions.add(s);
 			
-			r = new RecipeADT(data[0], data[1], data[2], data[3], data[4], ingredients, directions);
-			recipes.insert(data[0], r);
+//			r = new RecipeADT(data[0], data[1], data[2], data[3], data[4], ingredients, directions);
+			recipes.insert(name, r);
 		}
 		reader.close();
+	}
+	
+	public static void getInput() {
+       Scanner in = new Scanner(System.in);
+       String input = in.next();
+       System.out.println(recipes.find("Chicken With Spiced Rice"));
+       
 	}
 	
 	public static void main(String[] args) {
@@ -65,9 +85,9 @@ public class Main {
 		
 		} catch (FileNotFoundException a) {
 			// can't find file, parse from website
-			System.out.println("File not found, downloading data...");
+			System.out.println("File not found, parsing data...");
 			Parser.parseHTML();
-			System.out.println("Downloaded successfully.");
+			System.out.println("Loaded successfully.");
 			
 			try {
 				// save file for future use
@@ -83,5 +103,7 @@ public class Main {
 			System.out.println("Something went wrong with downloading data.");
 			b.printStackTrace();
 		}
+		
+		getInput();
 	}
 }
--- Parser.java
+++ Parser.java
@@ -1,4 +1,5 @@
 package recipebook;
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -10,46 +11,69 @@ public class Parser {
 	
 	public static void parseHTML () {
 		RecipeADT recipe;
-		String name, description, prepTime, totalTime, servings;
+		String directory, name, description, prepTime, totalTime, servings;
 		ArrayList<String> ingredients;
 		ArrayList<String> directions;
 		String[] ingredientsArray;
+		Document doc;
+		Elements meta, dirs, li;
 		
-		String url;
-		for (int i = 10; i < 520000; i++) {
-			try {
-				url = "http://www.food.com/recipeprint.do?rid="+Integer.toString(i);
-				Document doc = Jsoup.connect(url).get();
+		String[] dir = new String[]{"http:", "http: (2)", "http: (3)", "http: (4)", "http: (5)"};
 				
-				// pull out name of recipe
-				name = doc.title().split(" Recipe")[0];
-				
-				// find 'meta' tags
-				Elements meta = doc.select("meta");
-				// pull out ingredients
-				ingredients = new ArrayList<String>();
-				ingredientsArray = meta.get(2).attr("content").split(" recipe")[0].split("\\,");
-				for (String s : ingredientsArray) ingredients.add(s);
-				
-				// pull out description text
-				description = meta.get(3).attr("content");
-				// remove newlines from text
-				description = description.replaceAll("\n", "");
-				description = description.replaceAll("\r", " ");
-				// pull out directions
-				directions = new ArrayList<String>();
-				Elements dirs = doc.getElementsByTag("ol").get(0).getElementsByTag("span");
-				for (Element d : dirs) directions.add(d.text());
-				
-				Elements li = doc.getElementsByTag("li");
-				prepTime = li.get(0).text().split(": ")[1];
-				totalTime = li.get(1).text().split(": ")[1];
-				servings = li.get(2).text().split(": ")[1];
-				
-				recipe = new RecipeADT(name, prepTime, totalTime, servings, description, ingredients, directions);
-				Main.recipes.insert(name, recipe);
-				
-			} catch (Exception e) {} // continue on next loop if a recipe isn't valid
+		System.out.println("Progress:");
+		for (int a = 0; a < 5; a++) {
+			System.out.print(20*a);
+			for (int b = 1; b <= 1000; b++) {
+//			for (int b = 1; b <= 100000; b++) {
+				// draw a progress bar
+				// save every 5000 recipes
+				if (b%5000==0) {
+					System.out.print(".");
+					try {
+						Main.save();
+						Main.recipes = new RBT<String, RecipeADT>();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+				try {
+
+					directory = "/home/ben/Desktop/Workspace/HTML/" + dir[a] 
+								+ "/www.food.com/recipeprint.do?rid=" 
+								+ Integer.toString(a*100000+b);
+					
+					doc = Jsoup.parse(new File(directory), "UTF-8", "");
+					
+					// pull out name of recipe
+					name = doc.title().split(" Recipe")[0];
+					
+					// find 'meta' tags
+					meta = doc.select("meta");
+					// pull out ingredients
+					ingredients = new ArrayList<String>();
+					ingredientsArray = meta.get(2).attr("content").split(" recipe")[0].split("\\,");
+					for (String s : ingredientsArray) ingredients.add(s);
+					
+					// pull out description text
+					description = meta.get(3).attr("content");
+					// remove newlines from text
+					description = description.replaceAll("\n", "");
+					description = description.replaceAll("\r", " ");
+					// pull out directions
+					directions = new ArrayList<String>();
+					dirs = doc.getElementsByTag("ol").get(0).getElementsByTag("span");
+					for (Element d : dirs) directions.add(d.text());
+					
+					li = doc.getElementsByTag("li");
+					prepTime = li.get(0).text().split(": ")[1];
+					totalTime = li.get(1).text().split(": ")[1];
+					servings = li.get(2).text().split(": ")[1];
+					
+					recipe = new RecipeADT(name, prepTime, totalTime, servings, description, ingredients, directions);
+					Main.recipes.insert(name, recipe);
+					
+				} catch (Exception e) {} // continue on next loop if a recipe isn't valid
+			}
 		}
 	}
 }
